c5d92fccf4e4e0a6580e6bc825d5d192a614bc19
RhysMJ <rhysmjones11@gmail.com>
2018-06-28 13:37:21 +0100

    Example Piece
    This is only an example program for the moment which must be used in order for the TCP module to be able to connect to the other computer. This will be removed/replaced once I've created a better program/system for the server/hosting computer.

--- server.py
+++ server.py
@@ -1,17 +1,24 @@
 import socket
-data =0
-TCP_IP = '192.168.1.8'
-TCP_PORT = 5005
-BUFFER_SIZE = 20  # Normally 1024, but we want fast response
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.bind((TCP_IP, TCP_PORT))
-s.listen(1)
+import tcp
 
-conn, addr = s.accept()
-print ('Connection address:', addr)
-while data != "stop":
-    data = conn.recv(BUFFER_SIZE).decode()
-    if not data: break
-    print ("received data:", data)
-    conn.send(data.encode())  # echo
-conn.close()
+def server():
+    data =0
+    TCP_IP = tcp.getmyip()
+    TCP_PORT = int(input("Enter port: "))
+    BUFFER_SIZE = 20  # Normally 1024, but we want fast response
+
+    conn = tcp.bind(TCP_IP, TCP_PORT)
+
+    while data != "stop":
+        data = tcp.receive(conn, BUFFER_SIZE)
+        if not data:
+            conn = tcp.bind(TCP_IP, TCP_PORT)
+        else:
+            print ("received data:", data)
+            
+            tcp.send(data, conn)
+            #conn.send(data.encode())  # enumerate
+            
+    conn.close()
+
+server()
