18d09249bc953ca5014087390a54c3a42aef6e6e
RhysMJ <rhysmjones11@gmail.com>
2018-07-01 17:13:38 +0100

    Update to v0.91
    Small changes for easier use when using this program with another.

--- server.py
+++ server.py
@@ -1,24 +1,30 @@
-import socket
-import tcp
+## Example use of tcp v0.91 ##
+## R. M. Jones ##
+## 01/07/2017 ##
+
+import tcp  #imoprts the module that is included with this file
 
 def server():
+    
     data =0
-    TCP_IP = tcp.getmyip()
-    TCP_PORT = int(input("Enter port: "))
+    TCP_IP = tcp.getmyip() #Gets your private ip address
+    TCP_PORT = int(input("Enter port: ")) #The port that will be open
     BUFFER_SIZE = 20  # Normally 1024, but we want fast response
 
     conn = tcp.bind(TCP_IP, TCP_PORT)
 
-    while data != "stop":
-        data = tcp.receive(conn, BUFFER_SIZE)
-        if not data:
-            conn = tcp.bind(TCP_IP, TCP_PORT)
-        else:
-            print ("received data:", data)
-            
-            tcp.send(data, conn)
-            #conn.send(data.encode())  # enumerate
-            
-    conn.close()
+    
+    data = tcp.receive(conn, BUFFER_SIZE)
+    if not data:
+        conn = tcp.bind(TCP_IP, TCP_PORT)
+    else:
+        return "received data: " + data, conn
 
-server()
+
+if __name__ == "__main__":
+    info = 0
+    while info != "stop":
+        info, s = server()
+        print(info)
+        tcp.send(data, s)    
+        s.close()
